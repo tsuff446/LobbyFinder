a5202342a6ff719b111f61871b922c71ebcdcb2b
Thomas446 <36467286+Thomas446@users.noreply.github.com>
2020-04-21 16:52:58 -0500

    Initial Formatting

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="titleAlign" style="margin-top: 1%; ">
+
+    <div style="width:25%;height:10%; display:inline-block;vertical-align:top;"></div>
+    <!-- Centers the header !-->
+    <div style="text-align: center; display:inline-block;width:50%;">
+    <h1>Live NU Smash Lobbies</h1>
+    <form>
+        <input type="submit" value="Create an Arena">
+    </form>
+    </div>
+
+    <div id="OnlineBox">
+        Online Players:
+        <div id="PlayerList">test</div>
+    </div>
+
+</div>
+<div id="ArenasBox" style="background-color:red">this</div>
+
+</body>
+</html>
